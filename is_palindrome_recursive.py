94c25e0cd53afe9de5f70011a25daf666e36f384
Stieve.Chen <chensunmac@gmail.com>
2017-04-07 23:54:36 -0400

    Recursive and Iterative Is_Palindrome

---
+++ is_palindrome_recursive.py
@@ -0,0 +1,18 @@
+# check if is palindrome using a recursive way 
+# and nonrecersive way
+
+def is_palindrome(s):
+    if s == '':
+        return True
+    else:
+        if s[0] == s[-1]:
+            return is_palindrome(s[1:-1])
+        else:
+            return False
+        
+        
+def iter_palindrome(s):
+    for i in range( 0, len(s) / 2 ):
+        if s[ i ] != s[ - ( i + 1 ) ]:
+            return False
+        return True
