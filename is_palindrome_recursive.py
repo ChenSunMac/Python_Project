2def5fc1947b2ccfec462615700af8bc14877d00
Stieve.Chen <chensunmac@gmail.com>
2017-04-07 23:56:05 -0400

    Added Fibonacci

--- is_palindrome_recursive.py
+++ is_palindrome_recursive.py
@@ -16,3 +16,10 @@ def iter_palindrome(s):
         if s[ i ] != s[ - ( i + 1 ) ]:
             return False
         return True
+    
+def fibonacci(n):
+    current = 0
+    after = 1
+    for i in range(0,n):
+        current, after = after, current + after
+    return current
