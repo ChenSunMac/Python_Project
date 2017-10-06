d54c8ac276e9b01851ad7c9edeb755030e9e7061
Chain <chensunmac@gmail.com>
2017-10-06 14:39:14 -0400

    machinelearning

---
+++ machinelearning/fruit.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 06 10:51:13 2017
+
+@author: chens
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+fruits = pd.read_table('fruit_data_with_colors.txt')
+
+
+# create a mapping from fruit label value to fruit name to make results easier to interpret
+lookup_fruit_name = dict(zip(fruits.fruit_label.unique(), fruits.fruit_name.unique()))   
+
+### Examine the data
+# plotting a scatter matrix
+from matplotlib import cm
+X = fruits[['height', 'width', 'mass', 'color_score']]
+y = fruits['fruit_label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+cmap = cm.get_cmap('gnuplot')
+scatter = pd.scatter_matrix(X_train, c= y_train, marker = 'o', s=40, hist_kwds={'bins':15}, figsize=(9,9), cmap=cmap)
+
+
+# plotting a 3D scatter plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+ax.scatter(X_train['width'], X_train['height'], X_train['color_score'], c = y_train, marker = 'o', s=100)
+ax.set_xlabel('width')
+ax.set_ylabel('height')
+ax.set_zlabel('color_score')
+plt.show()
+
+
+### Create train-test split
+# For this example, we use the mass, width, and height features of each fruit instance
+X = fruits[['mass', 'width', 'height']]
+y = fruits['fruit_label']
+
+# default is 75% / 25% train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+### Create Classifier Object
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors = 5)
+
+
+### Train the classifier using training data
+knn.fit(X_train, y_train)
+
+### Estimate the accuracy of the classifier on test data (future data)
+knn.score(X_test, y_test)
+
+### Using trained K-NN model to classify the new items 
+# first example: a small fruit with mass 20g, width 4.3 cm, height 5.5 cm
+fruit_prediction = knn.predict([[20, 4.3, 5.5]])
+lookup_fruit_name[fruit_prediction[0]]
+
+
+
+
+
+
+
+
+
+
