dfdc2f7557f769ec736e04c2f6b341541286dd56
Chain <chensunmac@gmail.com>
2017-10-18 16:02:52 -0400

    new problem solved

---
+++ IBMoa/filter_passage_subString.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 14:04:01 2017
+
+@author: chens
+"""
+
+import re
+
+def clean_input(text):
+    #non-alphanumeric character should be ignored
+    text = re.sub('[^a-zA-Z\s]', '', text)
+    #Any other block of contiguous whitespace should be treated as a single space
+    #white space should be retained
+    text = re.sub(' +',' ',text)
+    #Leading and trailing whitespace should be ignored
+    text = text.strip(' \t\n\r')
+    # You probably want this too
+    text = text.lower()
+    return text
+
+def process(text):
+    #If they are also the same length, the earlier one in the input sequence should be kept.
+    # Using arrays (can use OrderedDict too, probably easier and nice, although below is clearer for you.
+    original_parts = text.split('|')
+    clean_parts = [clean_input(x) for x in original_parts]
+    original_parts_to_check = []
+    ignore_idx = []
+    for idx, ele in enumerate(original_parts):
+        if idx in ignore_idx:
+            continue
+        #The case of alphabetic characters should be ignored
+        if len(ele) < 2:
+            continue
+        #Duplicates must also be filtered -if two passages are considered equal with respect to the comparison rules listed above, only the shortest should be retained.
+        if clean_parts[idx] in clean_parts[:idx]+clean_parts[idx+1:]:
+            indices = [i for i, x in enumerate(clean_parts) if x == clean_parts[idx]]
+            use = indices[0]
+            for i in indices[1:]:
+                if len(original_parts[i]) < len(original_parts[use]):
+                    use = i
+            if idx == use:
+                ignore_idx += indices
+            else:
+                ignore_idx += [x for x in indices if x != use]
+                continue
+        original_parts_to_check.append(idx)
+    # Doing the text in text matching here. Depending on size and type of dataset,
+    # Which you should test as it would affect this, you may want this as part
+    # of the function above, or before, etc. If you're only doing 100 bits of
+    # data, then it doesn't matter. Optimize accordingly.
+    text_to_return = []
+    clean_to_check = [clean_parts[x] for x in original_parts_to_check]
+    for idx in original_parts_to_check:
+        # This bit can be done better, but I have no more time to work on this.
+        if any([(clean_parts[idx] in clean_text) for clean_text in [x for x in clean_to_check if x != clean_parts[idx]]]):
+            continue
+        text_to_return.append(original_parts[idx])
+    #The retained passages should be output in their original form (identical to the input passage), and in the same order.
+    return '|'.join(text_to_return)
+
+assert(process('IBM cognitive computing|IBM "cognitive" computing is a revolution| ibm cognitive computing|\'IBM Cognitive Computing\' is a revolution?') ==
+       'IBM "cognitive" computing is a revolution')
+print(process('IBM cognitive computing|IBM "cognitive" computing is a revolution| ibm cognitive computing|\'IBM Cognitive Computing\' is a revolution?'))
+assert(process('IBM cognitive computing|IBM "cognitive" computing is a revolution|the cognitive computing is a revolution') ==
+       'IBM "cognitive" computing is a revolution|the cognitive computing is a revolution')
+print(process('IBM cognitive computing|IBM "cognitive" computing is a revolution|the cognitive computing is a revolution'))
--- IBMoa/streaming_analytic_service.py
+++ IBMoa/streaming_analytic_service.py
@@ -5,7 +5,7 @@ IBM OA - streaming analytic service for online retailor data anlytic
 Each line of the input includes a string 'date,number,item'
 The output would be 'data,totalNumberOfAllItems,average,NumberOfCategories'
 Example Input:
-    '2017-06-02,5,Apples\n2017-06-02,2,pears,2017-06-03,3,pineapples'
+    '2017-06-02,5,Apples\n2017-06-02,2,pears\n2017-06-03,3,pineapples'
 Example Output:
     '2017-06-02,7,3.5,2\n2017-06-03,3,3.00,1'
 Use Dict, split and so on
@@ -13,4 +13,43 @@ Note one can use round(float number, 2) to result a number less than 2 decimal d
 One can use print("%.2f" % float number) to get a exact 2 decimal digits form 
 @author: chens
 """
+
+test_input = '2017-06-02,5,Apples\n2017-06-02,2,pears\n2017-06-03,3,pineapples'
+Data_map = dict()
+segment_list = test_input.split('\n')
+#segment_list = ['2017-06-02,5,Apples', '2017-06-02,2,pears', '2017-06-03,3,pineapples']
+"""MAPPER PART"""
+for line in segment_list:
+    date_num_item = line.split(',')
+    # if new Date, then create a instance in the dict.
+    if date_num_item[0] not in Data_map:
+        # construct the dict for the entry
+        Data_map[date_num_item[0]] = {date_num_item[2].lower() : int(date_num_item[1])}
+        # initialize the 'TOTAL_NUMBER' part for each new entry
+        Data_map[date_num_item[0]]['TOTAL_NUMBER'] = 0
+        Data_map[date_num_item[0]]['TOTAL_NUMBER'] += int(date_num_item[1])
+        # initialize the 'ITEMS_NUMBER' part for each new entry
+        Data_map[date_num_item[0]]['ITEMS_NUMBER'] = 1
+    # if has such Date, then we need to insert the data in the dict
+    else:
+        # if item not exist
+        if date_num_item[2].lower() not in Data_map[date_num_item[0]]:
+            Data_map[date_num_item[0]][date_num_item[2].lower()] = int(date_num_item[1])
+            Data_map[date_num_item[0]]['TOTAL_NUMBER'] += int(date_num_item[1])
+            Data_map[date_num_item[0]]['ITEMS_NUMBER'] += 1
+            # if item already exist
+        else:
+            Data_map[date_num_item[0]][date_num_item[2].lower()] += int(date_num_item[1])
+            Data_map[date_num_item[0]]['TOTAL_NUMBER'] += int(date_num_item[1])
+        
+            
+"""REDUCER PART"""
+
+for key in sorted(Data_map.keys()):
+    total_number = Data_map[key]['TOTAL_NUMBER']
+    item_number = Data_map[key]['ITEMS_NUMBER']
+    average = float(total_number)/float(item_number)
+    print("%s,%d,%.2f,%d" % (key, total_number, average, item_number))
+
+
 
